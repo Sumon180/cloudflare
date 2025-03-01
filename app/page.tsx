45b40deb34d085a349e5e38e4c50cc81a4ea4cd1
Sumon Hossain <sumonengr.96@gmail.com>
2025-03-01 20:49:35 +0600

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import NestedCard01 from "./Components/NestedCard01";
 import NestedCard02 from "./Components/NestedCard02";
 
-export const runtime = "edge";
-
 const Home = () => {
   return (
     <div>
